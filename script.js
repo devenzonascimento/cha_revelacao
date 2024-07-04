613b75a62ec66e09e4f30252f43ef79842448c07
devenzonascimento <dev.enzody@gmail.com>
2024-07-03 22:59:07 -0300

    adicionado apresentação

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".toggle").addEventListener("click", () => {
+  console.log("olá")
+  document.querySelector(".presentation").style.display = "none"
+  document.querySelector(".menu").style.display = "flex"
+})
+
+function toggleDisplays() {
+  console.log("olá")
+  document.querySelector(".presentation").style.display = "none"
+  document.querySelector(".menu").style.display = "flex"
+}
